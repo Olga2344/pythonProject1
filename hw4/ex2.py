6d454625a1840c1eb38a6d791f387aefc7320565
olga <arriba-en@ya.ru>
2022-01-25 19:20:17 +0300

    4 урок с доделками 2 задания

--- hw4/ex2.py
+++ hw4/ex2.py
@@ -2,28 +2,45 @@
 # принимать на вход натуральное и возвращать соответствующее простое число. Проанализировать скорость и сложность
 # алгоритмов.
 
-import sys
-
-n=20
-count=1
-num_list = [2]
-test_num_list=[]
-for i in range(3, sys.maxsize**10, 2):
-    j = i * 2
-    test_num_list.append(j + i)
-    test_num_list.append(i**2)
-    if i>5 and i%5==0:
-        test_num_list.append(i)
-
-    if i not in test_num_list:
-        num_list.append(i)
-        count+=1
-
-    if count == n:
-        print(i)
-        break
+import sys, cProfile
 
 
-print(num_list)
-print(test_num_list)
+def prime(num):
+    count = 1
+    num_list = [2]
+    test_num_list = []
+    for i in range(3, sys.maxsize ** 10, 2):
+        j = i * 2
+        test_num_list.append(j + i)
+        test_num_list.append(i ** 2)
+        if i > 5 and i % 5 == 0:
+            test_num_list.append(i)
 
+        if i not in test_num_list:
+            num_list.append(i)
+            count += 1
+
+        if count == num:
+            return i
+
+u_number=20
+print(prime(u_number))
+cProfile.run('prime(u_number)')
+
+'''
+
+100 loops, best of 5: 28.6 nsec per loop
+1000 loops, best of 5: 21 nsec per loop
+
+99 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 ex2.py:8(prime)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+       95    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+'''
